83cbd2297b28cc4957bfee9fc998e0f8a256cf9e
sajinandrey <sajinandrey@example.com>
2023-04-20 19:11:25 +0300

    упражнения 1 лекция ввод в программирования

---
+++ examples_5/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Ведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "андрей")
+{
+
+    Console.WriteLine("Здраствуй мой повелитель");
+
+}
+else 
+{
+    Console.Write("Добрый день, ");
+    Console.WriteLine(username);
+
+}
